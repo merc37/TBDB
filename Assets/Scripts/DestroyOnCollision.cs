6f186e9240c35efd244515abbf2432c00168c9fc
Gabriel <gabeajh@gmail.com>
2017-05-15 00:12:56 -0400

    Adds destroy script and sprite look direction

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour {
 
-	void OnCollisionEnter2D(Collision2D coll) {
+	void OnCollisionStay2D(Collision2D coll) {
 		if(coll.gameObject.tag == "Block"){
 			Destroy(this.gameObject);
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D coll) {
+	void OnTriggerStay2D(Collider2D coll) {
 		if(coll.gameObject.tag == "Block"){
 			Destroy(this.gameObject);
 		}
---
+++ Assets/Scripts/SpriteLookDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLookDirection : MonoBehaviour {
+
+	[SerializeField]
+	private Rigidbody2D rigidBody;
+
+	void FixedUpdate() {
+		float angle = Mathf.Atan2(rigidBody.velocity.x, rigidBody.velocity.y) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+	}
+}
