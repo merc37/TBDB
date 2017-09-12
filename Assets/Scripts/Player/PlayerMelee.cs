94cb9201cd47d2a8fb8676d1dc88eed0d4f7222e
Gabriel <gabeajh@gmail.com>
2017-09-12 15:42:49 -0400

    Add gun script. Began work on audio

---
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetButtonDown("Melee")) {
+
+        }
+	}
+
+}
