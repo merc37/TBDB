0ee48338971f4a2a2fa326bc24d662bbf0da7ae5
Gabriel <gabeajh@gmail.com>
2019-01-04 19:17:34 -0500

    Player no longer has acceleration

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,22 +6,16 @@ namespace Player {
 	public class PlayerMovement : MonoBehaviour {
         
         [SerializeField]
-        private float maxRunSpeed = 30;
+        private float maxRunSpeed = 15;
         [SerializeField]
-        private float maxWalkSpeed = 10;
-        [SerializeField]
-        private float runAcceleration = 1;
-        [SerializeField]
-        private float walkAcceleration = .3f;
+        private float maxWalkSpeed = 5;
 
         private new Rigidbody2D rigidbody;
         private GameObjectEventManager eventManager;
 
         private float currentMaxSpeed;
-        private float currentAcceleration;
 
         private Vector2 directionVector = Vector2.zero;
-        private Vector2 newVelocity = Vector2.zero;
 
         private bool walking;
         private bool rolling;
@@ -37,45 +31,15 @@ namespace Player {
         void Start() {
             eventManager.TriggerEvent("SendMovementSpeed", new ParamsObject(maxRunSpeed));
         }
-
-        void Update() {
-            directionVector.Set(Input.GetAxisRaw("HorizontalMovement"), Input.GetAxisRaw("VerticalMovement"));
-            walking = Input.GetButton("Walk");    
-        }
         
         void FixedUpdate() {
             if(!rolling) {
-                
-                currentMaxSpeed = walking ? maxWalkSpeed : maxRunSpeed;
-                currentAcceleration = walking ? walkAcceleration : runAcceleration;
 
-                newVelocity = rigidbody.velocity;
-                if(directionVector.x != 0 && directionVector.x == -Mathf.Sign(newVelocity.x)) {
-                    newVelocity.x = 0;
-                }
-                if(directionVector.y != 0 && directionVector.y == -Mathf.Sign(newVelocity.y)) {
-                    newVelocity.y = 0;
-                }
-                if(directionVector.x == 0) {
-                    if(Mathf.Abs(newVelocity.x) <= currentAcceleration) {
-                        newVelocity.Set(0, newVelocity.y);
-                    }
-                    if(Mathf.Abs(newVelocity.x) > currentAcceleration) {
-                        directionVector.Set(-Mathf.Sign(newVelocity.x), directionVector.y);
-                    }
-                }
-                if(directionVector.y == 0) {
-                    if(Mathf.Abs(newVelocity.y) <= currentAcceleration) {
-                        newVelocity.Set(newVelocity.x, 0);
-                    }
-                    if(Mathf.Abs(newVelocity.y) > currentAcceleration) {
-                        directionVector.Set(directionVector.x, -Mathf.Sign(newVelocity.y));
-                    }
-                }
-                newVelocity += directionVector.normalized * currentAcceleration;
-                newVelocity = Vector2.ClampMagnitude(newVelocity, currentMaxSpeed);
+                currentMaxSpeed = Input.GetButton("Walk") ? maxWalkSpeed : maxRunSpeed;
 
-                rigidbody.velocity = newVelocity;
+                directionVector.Set(Input.GetAxisRaw("HorizontalMovement"), Input.GetAxisRaw("VerticalMovement"));
+
+                rigidbody.velocity = Vector2.ClampMagnitude(directionVector.normalized * currentMaxSpeed, currentMaxSpeed);
 
             }
         }
